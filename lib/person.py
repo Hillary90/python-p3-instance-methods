1f111310233426f7234c708f52f93b7c0c1d7fcd
Hillary Tanui <hillarytanui80@gmail.com>
2025-11-13 19:42:12 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+    
 
     #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+    
+hillary = Person()
+hillary.talk()  # Hello World!
+hillary.walk()  # The person is walking.
